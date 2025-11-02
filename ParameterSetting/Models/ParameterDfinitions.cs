1cff21d58b7b174cb97c84d233647e411f65f039
John Doe <johndoe@example.com>
2025-11-02 15:41:58 +0900

    プロジェクト ファイルを追加します。

---
+++ ParameterSetting/Models/ParameterDfinitions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamEditor.Models
+{
+    public class ParameterDefinition
+    {
+        public string Name { get; set; } = "";
+        public string Type { get; set; } = "";
+        public string Description { get; set; } = "";
+        public double[]? Range { get; set; }
+        public List<string>? Values { get; set; }
+        public string? Unit { get; set; }
+    }
+
+    public class SchemaRoot
+    {
+        public List<ParameterDefinition> Parameters { get; set; } = new();
+    }
+}
---
+++ ParameterSetting/Models/SchemaLoader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace ParamEditor.Models
+{
+    public static class SchemaLoader
+    {
+        public static SchemaRoot Load(string path)
+        {
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+
+            using var reader = new StreamReader(path);
+            return deserializer.Deserialize<SchemaRoot>(reader);
+        }
+    }
+}
